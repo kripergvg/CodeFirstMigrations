bd916bcfacd4a4d0b0d4760fef4763cc4a812b4a
vladimiriv <wkololo_4ever@mail.ru>
2014-12-08 15:37:56 +0300

    Adding

---
+++ CodeFirstMigrations/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using CodeFirstMigrations.Migrations;
+
+namespace MigrationsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<BlogContext, Configuration>());
+            using (var db = new BlogContext())
+            {
+                db.Blogs.Add(new Blog { Name = "Another Blog " });
+                db.SaveChanges();
+
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine(blog.Name);
+                }
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+    //Data Source=(localdb)\mssqllocaldb;Initial Catalog=MigrationsDemo.BlogContext;Integrated Security=True;MultipleActiveResultSets=True
+}
